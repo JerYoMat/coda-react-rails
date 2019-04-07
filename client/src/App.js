6496f005e4a0246be5443482457589033a248b46
JerYOMat <jeremy.emata@gmail.com>
2019-04-07 17:53:13 -0400

    stripped everything as test

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-          <p>
-           Made IT!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+
+const App = () => <div>TEST</div>
+
 
 export default App;
