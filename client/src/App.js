97cab3c9c7f2a5bf790888feeed9ed6ef62c3f65
JerYOMat <jeremy.emata@gmail.com>
2019-04-07 17:46:47 -0400

    removed icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,9 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           Made IT!
           </p>
           <a
             className="App-link"
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users
-      resources :industries, only: [:index, :show]
-      resources :companies do 
-        resources :financial_periods
-      end 
-    end
-  end
+  resources :companies 
 end 
