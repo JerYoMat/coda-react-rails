1895394bdebf6a4b4ddd04f538036e3fb922baec
JerYOMat <jeremy.emata@gmail.com>
2019-04-07 19:07:11 -0400

    added companies controller with index action

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,19 @@
+class CompaniesController < ApplicationController
+  include ERB::Util
+
+  def index
+    @companies = Company.limit(100)
+    render json: @companies
+  end 
+
+  def create 
+   @company = Company.new 
+   @company.companyname = params['companyname']
+   @company.primarysymbol = params['primarysymbol']
+   @company.primaryexchange = params['primarysymbol']
+   @company.save 
+   render json: @company 
+  end 
+
+ 
+end
