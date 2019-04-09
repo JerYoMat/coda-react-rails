b94a36c55e6f9ec939259b8e2c5cea970a548682
JerYOMat <jeremy.emata@gmail.com>
2019-04-09 01:15:18 -0400

    changed production to ssl only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end 
 
   private 
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
