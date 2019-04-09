c59d6c7c9f51006d0ef256b70215f7b80b56a6a2
JerYOMat <jeremy.emata@gmail.com>
2019-04-09 17:59:40 -0400

    added loga as svg and changed color to match light-blue

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import './Navbar.scss'
-
+import { ReactComponent as Logo } from '../coda.svg';
 const Navbar = ({ user }) => {
   
   return (
     <div className='navbar-container'>
       <Link to='/'>
-      <span className='logo'>Logo</span>
+      <span className='logo'><Logo className='app-logo'/></span>
       </Link>
       <Link to='/simulator'>
         <span className='nav-link'>Simulator</span>
