18579951da2a5058213b7809f2a795dcf7a35493
JerYOMat <jeremy.emata@gmail.com>
2019-04-08 21:14:57 -0400

    added welcome card

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,7 +5,12 @@ import CardStack from '../components/CardStack';
 const HomePage = () => {
   return (
     <div>
-      <div className='banner-container'>T</div>
+      <div className='banner-container'>
+        <div className='welcome-card col-md-5 offset-md-2'>
+          <h2>Welcome to CoDA</h2>
+          <p>A data playground for credit analysts</p>
+        </div>
+      </div>
       <CardStack />
     </div>
   )
