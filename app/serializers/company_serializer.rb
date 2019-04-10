437478097490bbbf714d00d303033425baa614b1
JerYOMat <jeremy.emata@gmail.com>
2019-04-10 14:59:08 -0400

    reset company serializer to just returning industry id and sector,  can be handled more efficiently than using 3 serializers for each item

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,3 +1,3 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :company_name, :primarysymbol, :primaryexchange, :industry_id, :sector_id
+  attributes :id, :companyname, :primarysymbol, :primaryexchange, :industry_id, :sector_id
 end
