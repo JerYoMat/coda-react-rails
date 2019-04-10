bc00d800db31056c695bfa65865faad060f3d624
JerYOMat <jeremy.emata@gmail.com>
2019-04-10 01:24:11 -0400

    added search results component to companies page

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import './SearchResults.scss';
+import { Link } from '@reach/router';
 
-const SearchResults = ({ companies }) => (
- <div className='search-results-container'>
-   { !companies ? 
-      <div>Nothing here yet</div>
-      : <div>Placeholder</div>
-   }
- </div>
+
+const SearchResults = ({ companies }) => {
+ if (!companies) {
+   return <div></div>
+ }
+ 
+ return (
+  <div>
+    results ({companies.length})
+    {companies.map(company => (
+      <Link to={'/companies/' + company.id}>
+        <SearchCard key={company.id} company={company} />
+      </Link>
+    ))}
+    </div>
+)
+}
+
+
+const SearchCard = ({ company }) => (
+  <div className='card'>
+    <div className='search card-body'>
+      <h5 className='card-title'>
+        Company:{company.companyname}
+      </h5>
+      <p>
+        Ticker:  
+        <strong>
+         <span>  </span>
+         {company.primarysymbol}
+        </strong>
+      </p>
+      <p>
+        Exchange: 
+        <strong>
+          <span>  </span>
+          {company.primaryexchange}
+        </strong>
+      </p>
+    
+    </div>
+  </div>
 )
 
 export default SearchResults;
--- client/src/pages/CompaniesPage.js
+++ client/src/pages/CompaniesPage.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Search from '../components/Search';
 import SearchResults from '../components/SearchResults';
 import './CompaniesPage.scss'
+/*temp test file */
+import companies from '../components/search_test_file.js';
+
 
 const CompaniesPage = () => {
   return (
     <div className='companies-page-wrapper'>
       <div className='search-results-container'>
         <Search/>
-        <SearchResults />
+        <SearchResults companies={companies} />
       </div>
       <div className='sidebar-container'>Sidebar</div>
     </div>
