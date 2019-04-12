bcc87b9a2cf1f93bec89ff8b9dc83bc10fa486dc
JerYOMat <jeremy.emata@gmail.com>
2019-04-12 15:46:41 -0400

    t

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
- 
+  
       resources :companies, only: [:index]
       get '/companies/:ticker', to: 'companies#show'
-    
-
+      resources :users, only: [:create, :update, :destroy]
+      resources :favorites, only: [:create, :destroy]
+      post   '/login',   to: 'sessions#create'
+      delete '/logout',  to: 'sessions#destroy'
+  
 end 
