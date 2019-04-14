d053bdcd827215c1985d5cc76e740bc7801da274
JerYOMat <jeremy.emata@gmail.com>
2019-04-14 04:01:00 -0400

    connected CompanyProfilePage and mapped company from state list to component

--- client/src/pages/CompanyProfilePage.js
+++ client/src/pages/CompanyProfilePage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const CompanyProfilePage = () => {
+const CompanyProfilePage = ({ companyId, company }) => {
   return (
-    <div>Test</div>
+    <div>
+    <div>{companyId}</div>
+    <div>{company.companyname}</div>
+    </div>
   )
 }
+const mapState = (state, ownProps) => {
+  const companyId = parseInt(ownProps.companyId, 10);
+  return {
+    company:state.companies.list.find(el => el.id === companyId) 
+  }
+}
 
-export default CompanyProfilePage;
+export default connect(mapState)(CompanyProfilePage);
