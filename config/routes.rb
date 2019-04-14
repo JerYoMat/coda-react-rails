63d05a0caf66d86bea3fc330b2ef8d3d261b72ea
JerYOMat <jeremy.emata@gmail.com>
2019-04-14 18:52:20 -0400

    updated routes - incorrectly nested one

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
       namespace :api do 
             namespace :v1 do
-              resources :companies, only: [:index]
-              get '/companies/stocks/:ticker', to: 'companies#stock_data'
-              get '/companies/:ticker', to: 'companies#fins'
+                  get '/companies/:ticker', to: 'companies#fins'
+                  resources :companies, only: [:index]
+              
               resources :users, only: [:create, :update, :destroy]
               resources :favorites, only: [:create, :destroy]
               post   '/login',   to: 'sessions#create'
