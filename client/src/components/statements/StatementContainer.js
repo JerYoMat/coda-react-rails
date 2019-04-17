7fae4d10c89994d93895c3ca3da299c436cd4955
JerYOMat <jeremy.emata@gmail.com>
2019-04-17 01:33:19 -0400

    added open close to each statement

--- client/src/components/statements/StatementContainer.js
+++ client/src/components/statements/StatementContainer.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux'
+
 
 import Table from './sub-components/Table';
-const StatementContainer = ({ statements, companyId }) => (
+const StatementContainer = ({ statements }) => (
       <div className='financial-statements-container'>
           {
             statements && statements.hasOwnProperty('IS') ? 
               (
                 <React.Fragment>
                   <Table title="Income Statement" statement={statements['IS']} fiscalYears={statements['info']['fiscalyear']}/>
-                  <Table title="Balance Sheet" statement={statements['IS']} fiscalYears={statements['info']['fiscalyear']}/> <Table title="Cash Flow Statement" statement={statements['IS']} fiscalYears={statements['info']['fiscalyear']}/>
+                  <Table title="Balance Sheet" statement={statements['BS']} fiscalYears={statements['info']['fiscalyear']}/> <Table title="Cash Flow Statement" statement={statements['CF']} fiscalYears={statements['info']['fiscalyear']}/>
                 </React.Fragment>
               ) : 
               ("")
--- client/src/components/statements/sub-components/Table.js
+++ client/src/components/statements/sub-components/Table.js
@@ -1,43 +1,51 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronRight, faChevronDown } from "@fortawesome/free-solid-svg-icons"
 
 //takes statement, column number to show as props, dates or years for header, title
-const Table = ({ title, fiscalYears, statement}) => (
+const Table = ({ title, fiscalYears, statement}) => {
     
+  const [open, setOpen] = useState(false)
+  
 
-      <div>
-        <div>
-          {title} <i></i>
-        </div>
-          <table className="table table-bordered table-striped table-dark table-sm">
-            <thead>
-              <tr>
-                <th scope="col">FY:</th>
-                <th scope='col'>{fiscalYears[3]}</th>
-                <th scope='col'>{fiscalYears[2]}</th>
-                <th scope='col'>{fiscalYears[1]}</th>
-              </tr>
-            </thead>
-            <tbody>
-              
-              {Object.keys(statement).map((key) => {
-                const row = statement[key]
-                if (statement[key].length > 1) {
-                return (
-                  <tr key={key}>
-                  <td>{row[0]}</td>
-                  <td  className='statement-num'>{row[3]}</td>
-                  <td  className='statement-num'>{row[2]}</td>
-                  <td  className='statement-num'>{row[1]}</td>
-                  </tr>
-                )}
-              })}
+  return (
+  <div className='statement-table-wrapper' onClick={() => {setOpen(!open)}}>
+    <h5>
+      {title}  {
+        open ? (<FontAwesomeIcon icon={faChevronDown} />) 
+          : (<FontAwesomeIcon icon={faChevronRight} />)
+      }
+    </h5>
+    { open && (
+      <table display={open} className="table table-bordered table-striped table-dark table-sm">
+        <thead>
+          <tr>
+            <th scope="col">FY:</th>
+            <th scope='col'>{fiscalYears[3]}</th>
+            <th scope='col'>{fiscalYears[2]}</th>
+            <th scope='col'>{fiscalYears[1]}</th>
+          </tr>
+        </thead>
+        <tbody>
           
-            </tbody>
-          </table>
-        </div>
-
-
-)
+          {Object.keys(statement).map((key) => {
+            const row = statement[key]
+            if (statement[key].length > 1) {
+            return (
+              <tr key={key}>
+              <td>{row[0]}</td>
+              <td  className='statement-num'>{row[3]}</td>
+              <td  className='statement-num'>{row[2]}</td>
+              <td  className='statement-num'>{row[1]}</td>
+              </tr>
+            )}
+          })}
+      
+        </tbody>
+      </table>
+      )}
+    </div>
+  )
+}
 
 export default Table
