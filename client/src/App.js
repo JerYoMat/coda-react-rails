5e645a62198675d37ac352ced73ed3e54a4a7928
JerYOMat <jeremy.emata@gmail.com>
2019-04-26 20:50:40 -0400

    removed Companies page component

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Router } from '@reach/router';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-import CompaniesPage from './pages/CompaniesPage';
 import CompanyProfilePage from './pages/CompanyProfilePage';
+
 const App = () => {
- 
   return (
     <div>
     <Navbar />
     <Router>
       <HomePage path='/' />
-      <CompaniesPage path='/companies' />
       <CompanyProfilePage path='/companies/:companyId' />
     </Router>
     </div>
