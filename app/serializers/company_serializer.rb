94fe39102a881044b4797c9871f7c0d77a8aee51
JerYOMat <jeremy.emata@gmail.com>
2019-04-27 18:43:47 -0400

    created basic serializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,3 +1,3 @@
 class CompanySerializer < ActiveModel::Serializer
-  attributes :id, :companyname, :primarysymbol, :primaryexchange, :industry_id, :sector_id, :snapshot_link
+  attributes :id, :companyname, :primarysymbol, :primaryexchange
 end
---
+++ app/serializers/favorite_serializer.rb
@@ -0,0 +1,3 @@
+class FavoriteSerializer < ActiveModel::Serializer
+  attributes :company_id
+end
