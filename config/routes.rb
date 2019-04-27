5360311556bed0d250c342622a750ac75d31cdce
JerYOMat <jeremy.emata@gmail.com>
2019-04-27 03:27:59 -0400

    updated login and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
       namespace :api do 
             namespace :v1 do
                   resources :companies, only: [:index, :show]
-                  resources :users, only: [:create, :update, :destroy]
+                  resources :users, only: [:create]
                   resources :favorites, only: [:create, :destroy]
-                  post   '/login',   to: 'sessions#create'
+                  post   'auth/login',   to: 'authentication#authenticate'
                   delete '/logout',  to: 'sessions#destroy'
             end       
       end 
 end 
+
