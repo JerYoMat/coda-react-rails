ab1a699a3fb71344c25c236453cf80b67d63c906
JerYOMat <jeremy.emata@gmail.com>
2019-04-26 21:54:09 -0400

    moved Navbar into its own directory and stripped out styling

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import './Navbar.scss'
-import { ReactComponent as Logo } from '../coda.svg';
+import { ReactComponent as Logo } from '../../coda.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+
 const Navbar = () => {
   
   return (
     <div className='navbar-container'>
       <Link to='/'>
         <span className='logo'><Logo className='app-logo'/>CoDA</span>
+      </Link>
+      <Link to='/users/:id'>
+        <FontAwesomeIcon icon={faSignInAlt} />
       </Link>
     </div>
   )
