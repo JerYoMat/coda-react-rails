100cde430814aeb7792e7a638270d9025cf386ef
JerYOMat <jeremy.emata@gmail.com>
2019-04-26 21:15:39 -0400

    removed tabs not used in MVP

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,28 +2,13 @@ import React from 'react';
 import { Link } from '@reach/router';
 import './Navbar.scss'
 import { ReactComponent as Logo } from '../coda.svg';
-const Navbar = ({ user }) => {
+const Navbar = () => {
   
   return (
     <div className='navbar-container'>
       <Link to='/'>
-      <span className='logo'><Logo className='app-logo'/></span>
+        <span className='logo'><Logo className='app-logo'/>CoDA</span>
       </Link>
-      <Link to='/simulator'>
-        <span className='nav-link'>Simulator</span>
-      </Link>
-      <Link to='/compare'>
-        <span className='nav-link'>Compare</span>
-      </Link>
-
-      <Link to='/companies'>
-        <span className='nav-link'>Companies</span>
-      </Link>
-      
-      <Link to='/'>
-        <span className='nav-link'>Home</span>
-      </Link>
-     
     </div>
   )
 
