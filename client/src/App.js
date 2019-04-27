66ba70af9a1c57db51a6f3b4daab5cece3010460
JerYOMat <jeremy.emata@gmail.com>
2019-04-26 21:57:02 -0400

    updated import of Navbar to reflect new location

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import HomePage from './pages/HomePage';
 import CompanyProfilePage from './pages/CompanyProfilePage';
 
