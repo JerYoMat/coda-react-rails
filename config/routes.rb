ce1bd31b4e44c549038f2880bb3faa02be6cc550
JerYOMat <jeremy.emata@gmail.com>
2019-04-27 04:52:35 -0400

    replaced namespacing with scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-      namespace :api do 
-            namespace :v1 do
+      scope :api do 
+            scope :v1 do
                   resources :companies, only: [:index, :show]
                   resources :users, only: [:create]
                   resources :favorites, only: [:create, :destroy]
