078216c82145fca6dc4c2b9e3dcf2252c96e4127
JerYOMat <jeremy.emata@gmail.com>
2019-04-28 12:40:08 -0400

    set HMAC_SECRET to ENV var and standardized across production and development

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
   # secret to encode and decode token
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  HMAC_SECRET = ENV['AUTH_TOKEN_KEY']
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
