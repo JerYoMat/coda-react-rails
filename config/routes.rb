98e643a81dfa5f37ccb35ea06dd3283603f36063
JerYOMat <jeremy.emata@gmail.com>
2019-04-27 22:35:37 -0400

    added update, destroy routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
       scope :api do 
             scope :v1 do
                   resources :companies, only: [:index, :show]
-                  resources :users, only: [:create]
+                  resources :users, only: [:create, :update, :destroy]
                   resources :favorites, only: [:create, :destroy]
                   post   'auth/login',   to: 'authentication#authenticate'
                   delete '/logout',  to: 'sessions#destroy'
