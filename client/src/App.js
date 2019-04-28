7a3721d4d0bbd366499423c87dd588c955350e1d
JerYOMat <jeremy.emata@gmail.com>
2019-04-28 14:29:48 -0400

    added UserPage to Router section

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import './App.scss'
 import { Router } from '@reach/router';
 import HomePage from './pages/HomePage';
 import CompanyProfilePage from './pages/CompanyProfilePage';
+import UserPage from './pages/UserPage';
 import LoginCreateUser from './components/loginCreateUser/LoginCreateUser'; 
 import Navbar from './components/navbar/Navbar';
 
 class App extends Component {
-
   render() {
     return (
     <div>
@@ -16,9 +16,10 @@ class App extends Component {
       <div className='col-xl-2'></div>
         <div className='col-xl-8'>
           <Router> 
-          <HomePage path='/' />
+            <HomePage path='/' />
             <LoginCreateUser path='/login' />
             <CompanyProfilePage path='/companies/:companyId' />
+            <UserPage path='/users/:userId' />
           </Router>
         </div>
       <div className='col-xl-2'></div>
