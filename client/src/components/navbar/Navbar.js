840d768e5df4b710903b1a67de27eb405e6b2c73
JerYOMat <jeremy.emata@gmail.com>
2019-04-27 20:34:17 -0400

    updated navbar spacing to have logo left icon right and added max size to width

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './Navbar.scss'
+import { Link } from '@reach/router';
 import { ReactComponent as Logo } from '../../images/coda.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = ({ user, viewingCompany}) => {
   return (
-    <div className='navbar-container'>
-        <span className='logo'><Logo className='app-logo'/>CoDA</span>
-        <FontAwesomeIcon icon={faSignInAlt} />
-        <FontAwesomeIcon icon={faUserPlus} />
+    <div className='row'>
+      <div className='col-xl-2 nav-spacer'></div>
+      <div className='navbar-container col-xl-8'>
+          <Link to='/'>
+            <span className='logo'><Logo className='app-logo'/>CoDA</span>
+          </Link>
+          { user && (
+            <Link to='/users/:userId'>
+                <FontAwesomeIcon icon={faUser} className='access-icon' />
+            </Link>
+          )}
+          { !user && (
+          <Link to='/login'>
+            <FontAwesomeIcon icon={faSignInAlt} className='access-icon' size='lg' />
+          </Link>
+          )}
+      </div>
+      <div className='col-xl-2 nav-spacer'></div>
     </div>
   )
 }
 
-export default Navbar;
+const mapState = (state) => ({
+  user: state.user.info
+})
+
+export default connect(mapState)(Navbar);
