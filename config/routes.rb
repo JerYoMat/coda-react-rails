5806a0c5c87a0bfa948ecd6c70ab73fcc705c44f
JerYOMat <jeremy.emata@gmail.com>
2019-04-29 00:14:59 -0400

    added create_or_show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
       scope :api do 
             scope :v1 do
-                  resources :companies, only: [:index, :show]
+                  resources :companies, only: [:index]
                   resources :users, only: [:create, :update, :destroy]
                   resources :favorites, only: [:create, :destroy]
+                  get '/companies/:ticker', to: 'companies#show_or_create'
                   post   'auth/login',   to: 'authentication#authenticate'
                   delete '/logout',  to: 'sessions#destroy'
             end       
