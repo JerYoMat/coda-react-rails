3febfee79026548ad4fc57c07196f0d7b61701ba
JerYOMat <jeremy.emata@gmail.com>
2019-05-10 01:11:13 -0400

    specified default fields for a new user -moved indication of whether a user has a field setting saved to a boolean

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,54 @@ class User < ApplicationRecord
   has_secure_password
   serialize :custom_fields
   
-  def set_custom_fields
+  def set_custom_fields(type='save')
     self.custom_fields = {
       "iS" => {
-        'totalrevenue'=> ['Revenue', 1],
-        'costofrevenue'=> ['COGS', 0],
-        'grossprofit'=> ['Gross Profit', 0],
-        'researchdevelopmentexpense'=> ['R&D', 0],
-        'ebit'=> ['EBIT', 1],
-        'interestexpense'=> ['Interest Expense', 1],
-        'incomebeforetaxes'=> ['Pre-Tax Income', 0],
-        'netincome'=> ['Net Income', 1]
+        'totalrevenue'=> ['Revenue', true],
+        'costofrevenue'=> ['COGS', false],
+        'grossprofit'=> ['Gross Profit', false],
+        'researchdevelopmentexpense'=> ['R&D', false],
+        'ebit'=> ['EBIT', true],
+        'interestexpense'=> ['Interest Expense', true],
+        'incomebeforetaxes'=> ['Pre-Tax Income', false],
+        'netincome'=> ['Net Income', true]
+      },
+      "bS" => {
+        'cashandcashequivalents'=> ['Cash and Equivalents', true],
+        'inventoriesnet'=> ['Inventories', false],
+        'totalcurrentassets'=> ['Total Current Assets', true],
+        'propertyplantequipmentnet'=> ['PP&E, Net', false],
+        'intangibleassets'=> ['Intangible Assets', false],
+        'goodwill'=> ['Goodwill', false],
+        'totalassets'=> ['Total Assets', true],
+        'totalshorttermdebt'=> ['S-T Debt', true],
+        'totalcurrentliabilities'=> ['Total Current Liabilities', true],
+        'totallongtermdebt'=> ['L-T Debt', true],
+        'totalliabilities'=> ['Total Liabilities', true],
+        'retainedearnings'=> ['Retained Earnings', false],
+        'totalstockholdersequity'=> ['Total Shareholder Equity', true]
+      },
+      "cF" => {
+        'cashfromoperatingactivities'=> ['Cash from Operating Activities', true],
+        'cashfrominvestingactivities'=> ['Cash from Investing Activities', true],
+        'cashfromfinancingactivities'=> ['Cash from Financing Activities', true],
+        'capitalexpenditures'=> ['Capital Expenditures', false],
+        'cfdepreciationamortization'=> ['Depreciation & Amortization', false],
+        'netchangeincash'=> ['Net Change in Cash', false]
+      },
+      "pI" => {
+        'periodenddate'=> ['Fiscal Year End Date', true],
+        'fiscalyear'=> ['Fiscal Year', true],
+        'formtype'=> ['Form Type', true],
+        'audited'=>['Audited', false],
+        'original'=>['Original', false],
+        'amended'=> ['Amended', false],
+        'currencycode'=> ['Currency Code', true],
+        'usdconversionrate'=> ['Conversion Rate', true]
       }
     }
-    self.save
+    self.save unless type == 'no-save'
+    return self.custom_fields
   end 
 
 end 
