385edf2fe8ac288035cdc85fea878a3f962a3153
JerYOMat <jeremy.emata@gmail.com>
2019-05-10 01:50:10 -0400

    added new Statement components for Income Balance and CF -company data is available as well will need to decide what to do -chart is not currently being used -identifying data name, ticker etc needs to be added back

--- client/src/pages/CompanyProfilePage.js
+++ client/src/pages/CompanyProfilePage.js
@@ -1,32 +1,43 @@
 import React,{ useEffect } from 'react';
 import { connect } from 'react-redux';
 import { loadStatements} from '../actions';
-import StatementContainer from '../components/statements/StatementContainer';
-const CompanyProfilePage = ({ loading, error, company, statements, loadStatements }) => {
-  
+import Statement from '../components/statements/Statement';
+
+const CompanyProfilePage = ({ loading, error, company, data, loadStatements, fields }) => {
   if (loading) {
-    return <div>Loading...</div>;
+    return <div>Retrieving Data from EDGAR ONLINE ...</div>;
   }
 
   if (error) {
     return <div>Error: {error.message}</div>;}
   
-    if (statements === undefined) {
+  if (data === undefined || data === {}) {
     useEffect(() => {
-      // dispatch an action
-    loadStatements(company.id);
-    }, [statements]);
+        loadStatements(company.id);
+      }, [data]
+    );
   }
-
-  return (
-    <div></div>
-  )
+  if (data) {
+    const years = Object.keys(data)
+    return (
+      <div>
+      <Statement years={years} fields={fields['iS']} data={data}/>
+      <Statement years={years} fields={fields['bS']} data={data}/>
+      <Statement years={years} fields={fields['cF']} data={data}/>
+      </div>
+    )
+  }
+  return <div>Combining response with your customizations... </div>
+  
 }
 const mapState = (state, ownProps) => {
   const companyId = parseInt(ownProps.companyId, 10);
   return {
     company:state.companies.list.find(el => el.id === companyId),
-    statements: state.companies.statements[companyId]
+    data: state.companies.statements[companyId],
+    fields: state.user.customFields,
+    loading: state.companies.loadingStmnt,
+    error: state.companies.loadingStmntError
   }
 }
 
