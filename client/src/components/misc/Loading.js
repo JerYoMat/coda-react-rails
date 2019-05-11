4a42d5e0fd3ac6fb5097252ea51029b539ed50c5
JerYOMat <jeremy.emata@gmail.com>
2019-05-11 18:50:40 -0400

    added loading spinner

---
+++ client/src/components/misc/Loading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Loading.scss';
+
+const Loading = () => (
+  <div className='lds-spinner'>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+)
+
+export default Loading;
