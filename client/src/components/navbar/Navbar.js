491e5b6bbbc62378935aea654e52001ff4b29b17
JerYOMat <jeremy.emata@gmail.com>
2019-05-12 18:50:50 -0400

    added new Navbar template, old content in ONavbar until live test -connect and props still need to make it into new navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,40 +1,106 @@
 import React from 'react';
-import './Navbar.scss'
-import { Link } from '@reach/router';
-import { logout } from '../../actions';
-import { ReactComponent as Logo } from '../../images/coda.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
-import { connect } from 'react-redux';
-import DropDown from './sub-components/DropDown';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
-const Navbar = ({ user, logout}) => {
-  return (
-    <div className='row'>
-      <div className='col-xl-2 p-0 nav-spacer'></div>
-      <div className='navbar-container col-xl-8 p-0'>
-          <Link to='/'>
-            <span className='logo'><Logo className='app-logo'/>CoDA</span>
-          </Link>
-          { user && (
-            <DropDown userId={user.id} logout={logout} />
-          )}
-          { !user && (
-          <Link to='/login'>
-            <FontAwesomeIcon icon={faSignInAlt} className='login-icon' size='lg' />
-          </Link>
-          )}
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class Navbar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = event => {
+    this.setState({ auth: event.target.checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch checked={auth} onChange={this.handleChange} aria-label="LoginSwitch" />
+            }
+            label={auth ? 'Logout' : 'Login'}
+          />
+        </FormGroup>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Photos
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : undefined}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
       </div>
-      <div className='col-xl-2 p-0 nav-spacer'></div>
-    </div>
-  )
+    );
+  }
 }
 
-const mapState = state => ({
-  user: state.user.info
-})
-const mapDispatch = {
-  logout 
-}
 
-export default connect(mapState, mapDispatch)(Navbar);
+export default withStyles(styles)(Navbar);
