800ef554d65b2126bfc9d27d9cf9a235020366ea
JerYOMat <jeremy.emata@gmail.com>
2019-05-11 21:18:47 -0400

    customized chart to accept financial data and render desired field

--- client/src/components/charts/LineChart.js
+++ client/src/components/charts/LineChart.js
@@ -2,18 +2,55 @@ import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
-const LineChart = ({ years, finData }) => { 
-  
+const LineChart = ({years, data, fieldNames, displayName}) => { 
+  const test = years.map(year => {
+    const finNumber = Number(data[year][fieldNames].replace(/,/g, ''));
+    const finDate = Number(Date.parse(data[year]['periodenddate']))
+    return [finDate ,finNumber]
+  })
+  console.log(test)
+
+  Date.parse(data['2017']['periodenddate'])
+  const currency = data['2017']['currencycode']
+
+  const series = [{
+    name: displayName,
+    data: test
+  }]
+
   const options = {
+    chart: {
+      type: 'line'
+    },
     title: {
-      text: 'Sales Revenue over Time'
+      text: ''
+    },
+    subtitle: {
+      text:  displayName +' over Time'
     },
     xAxis: {
-      categories: years,
+      type: 'datetime'
     },
-    series: [{
-      data: finData.map(Number)
-    }]
+    yAxis: {
+      title: {
+        text: 'displayName'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    tooltip: {
+      valueSuffix: ` ${currency}`
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    },
+    series: series
   }
   return (
         <div>
--- client/src/pages/CompanyProfilePage.js
+++ client/src/pages/CompanyProfilePage.js
@@ -1,11 +1,25 @@
-import React,{ useEffect } from 'react';
+import React,{ useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { loadStatements} from '../actions';
 import Statement from '../components/statements/Statement';
+import LineChart from '../components/charts/LineChart';
+import FavoriteButton from '../components/favorites/FavoriteButton';
+import Loading from '../components/misc/Loading';
 
 const CompanyProfilePage = ({ loading, error, company, data, loadStatements, fields }) => {
+  const [chartFields, setChartFields] = useState('totalrevenue')
+  const handleFieldChange = (e) => {
+    const t = e.target.name
+    setChartFields(t)
+  }
+
   if (loading) {
-    return <div>Retrieving Data from EDGAR ONLINE ...</div>;
+    return (
+      <div>
+        <Loading />
+        <div>Retrieving Data from EDGAR ONLINE ...</div>
+      </div>
+    );
   }
 
   if (error) {
@@ -19,11 +33,20 @@ const CompanyProfilePage = ({ loading, error, company, data, loadStatements, fie
   }
   if (data) {
     const years = Object.keys(data)
+    const { companyname, primarysymbol, primaryexchange, industry, sector } = company; 
     return (
       <div>
-      <Statement years={years} fields={fields['iS']} data={data}/>
-      <Statement years={years} fields={fields['bS']} data={data}/>
-      <Statement years={years} fields={fields['cF']} data={data}/>
+        <FavoriteButton companyId={company.id} />
+          {companyname}
+          {primarysymbol}
+          {primaryexchange}
+          {industry}
+          {sector}
+        <button name='ebit' onClick={handleFieldChange}>Click me</button>
+        <LineChart years={years} data={data} fieldNames={chartFields} displayName={'Revenue'}/>
+        <Statement years={years} fields={fields['iS']} data={data}/>
+        <Statement years={years} fields={fields['bS']} data={data}/>
+        <Statement years={years} fields={fields['cF']} data={data}/>
       </div>
     )
   }
