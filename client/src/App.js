99ea4905f1df41545fd3de825ca3a24d736ab225
JerYOMat <jeremy.emata@gmail.com>
2019-05-12 18:47:59 -0400

    cut bootstrap related components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.scss'
 import { Router } from '@reach/router';
 import HomePage from './pages/HomePage';
 import CompanyProfilePage from './pages/CompanyProfilePage';
@@ -10,21 +9,15 @@ import Navbar from './components/navbar/Navbar';
 class App extends Component {
   render() {
     return (
-    <div>
+    <React.Fragment>
       <Navbar />
-      <div className='row'>
-      <div className='col-xl-2 p-0'></div>
-        <div className='col-xl-8 p-0'>
           <Router> 
             <HomePage path='/' />
             <LoginPage path='/login' />
             <CompanyProfilePage path='/companies/:companyId' />
             <UserPage path='/users/:userId' />
-          </Router>
-        </div>
-      <div className='col-xl-2 p-0'></div>
-      </div>
-    </div>
+          </Router>      
+    </React.Fragment>
     )
   }
 }
