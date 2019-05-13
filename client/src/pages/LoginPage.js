7af89a8e5b005ed3b4d93c37c65225312fb0c6a5
JerYOMat <jeremy.emata@gmail.com>
2019-05-13 05:37:22 -0400

    Rewrite: LoginPage is now the local state container for both login and signup forms

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,6 +1,91 @@
-import React from 'react';
-import LoginCreateUser from '../components/loginCreateUser/LoginCreateUser'
+import React, { useState } from 'react';
+import { login, signup } from '../actions';
+import Login from '../components/loginCreateUser/Login';
+import Signup from '../components/loginCreateUser/Signup';
+import { Redirect } from '@reach/router';
+import { connect } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
-const LoginPage = () => <LoginCreateUser />
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+  }
+});
 
-export default LoginPage;
+
+
+const LoginPage =({ classes, user, login, signup }) => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [showing, setShowing] = 
+  useState(0)  
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    login(email, password)
+  }
+  const handleSignup = (e) => {
+    e.preventDefault()
+    signup(username, email, password)
+  }
+
+  if (user) {
+    return <Redirect to="/" noThrow />;
+  }
+  
+  return (
+  <Paper square className={classes.root}>
+    <Tabs
+      value={showing}
+      onChange={() => setShowing(showing ? 0 : 1)}
+      variant="fullWidth"
+      indicatorColor="primary"
+      color="primary"
+    >
+      <Tab icon={<FontAwesomeIcon icon={faSignInAlt} size='lg' />} label="Sign in" />
+      <Tab icon={<FontAwesomeIcon icon={faUserPlus} size='lg' />} label="Sign up" />
+    </Tabs>
+    {showing === 0 && 
+      <Login 
+      email={email} 
+      password={password} 
+      setEmail={setEmail} 
+      setPassword={setPassword} 
+      handleSubmit={handleLogin}/>
+    }
+    {showing === 1 && 
+      <Signup 
+        username={username}
+        email={email}
+        passwordConfirmation={passwordConfirmation}
+        password={password}
+        setPasswordConfirmation={setPasswordConfirmation}
+        setUsername={setUsername}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleSubmit={handleSignup}
+      />}
+  </Paper>
+);
+}
+
+const mapState = state => ({
+  user: state.user.info,
+  error: state.user.error
+})
+
+const mapDispatch = {
+  login, signup
+}
+
+
+export default withStyles(styles)(connect(mapState, mapDispatch)(LoginPage));
