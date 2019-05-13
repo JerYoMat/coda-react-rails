fd0099367696eb25e5b0cdf7f27fb52b96710d02
JerYOMat <jeremy.emata@gmail.com>
2019-05-13 05:33:08 -0400

    refactor: Component to function

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Router } from '@reach/router';
 import HomePage from './pages/HomePage';
 import CompanyProfilePage from './pages/CompanyProfilePage';
@@ -6,20 +6,18 @@ import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
 import Navbar from './components/navbar/Navbar';
 
-class App extends Component {
-  render() {
-    return (
-    <React.Fragment>
-      <Navbar />
-          <Router> 
-            <HomePage path='/' />
-            <LoginPage path='/login' />
-            <CompanyProfilePage path='/companies/:companyId' />
-            <UserPage path='/users/:userId' />
-          </Router>      
-    </React.Fragment>
-    )
-  }
-}
+
+const App = () = (
+  <div>
+    <Navbar />
+    <Router>
+      <HomePage path='/' />
+      <LoginPage path='/login' />
+      <CompanyProfilePage path='/companies/:companyId' />
+      <UserPage path='/users/:userId' />
+    </Router>
+  </div>
+)
+
 
 export default App;
