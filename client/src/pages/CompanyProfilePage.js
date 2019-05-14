60d47747882c15279cb5ccbac078129b8616a336
JerYOMat <jeremy.emata@gmail.com>
2019-05-13 23:53:17 -0400

    rewrote as class component utilizing component lifecycle -after upgrade useEffect started throwing errors - conditional async is easiest to lay out in class component, problem appears to have been solved

--- client/src/pages/CompanyProfilePage.js
+++ client/src/pages/CompanyProfilePage.js
@@ -1,12 +1,13 @@
-import React,{ useEffect, useState } from 'react';
+import React,{ Component } from 'react';
 import { connect } from 'react-redux';
-import { loadStatements} from '../actions';
+import { loadStatements } from '../actions';
 import { withStyles } from '@material-ui/core/styles';
 import Statement from '../components/statements/Statement';
 import LineChart from '../components/charts/LineChart';
 import FavoriteButton from '../components/favorites/FavoriteButton';
 import Loading from '../components/misc/Loading';
 import Grid from '@material-ui/core/Grid';
+import NotFoundPage from './NotFoundPage';
 
 const styles = theme => ({
   root: {
@@ -18,65 +19,70 @@ const styles = theme => ({
   },
 });
 
-
-const CompanyProfilePage = ({ classes, loading, error, company, data, loadStatements, fields }) => {
-  if (loading) {
+class CompanyProfilePage extends Component {
+  componentDidMount() {
+    const { data, company, loadStatements  } = this.props
+    console.log(data)
+    console.log(company)
+    if (!data) {
+      loadStatements(company.id)
+    }
+  }
+  componentDidUpdate(prevProps) {
+    const { company, data, loadStatements } = this.props
+    if (!data) {
+     loadStatements(company.id)
+    }
+  }
+  render() {
+    const { classes, company, data, fields, loading, error} = this.props
+    if (loading || !data) {
+      return (
+        <div>
+          <Loading />
+          <div>Retrieving Data from EDGAR ONLINE ...</div>
+        </div>
+      );
+    }
+    if (!company) {
+      return <NotFoundPage />;
+    }
+    if (error) {
+      return <div>Error: {error.message}</div>
+    }
+    const years = Object.keys(data)
+    const { companyname, primarysymbol, primaryexchange, industry, sector } = company; 
     return (
-      <div>
-        <Loading />
-        <div>Retrieving Data from EDGAR ONLINE ...</div>
-      </div>
-    );
-  }
-  if (error) {
-    return <div>Error: {error.message}</div>
-  }
-  if (data === undefined || data === {}) {
-    useEffect(() => {
-        loadStatements(company.id);
-      }, [data]
-    );
-  }
-  if (!data) {
-    return <div>Combining response with your customizations... </div>
-  }
-
-  const years = Object.keys(data)
-  const { companyname, primarysymbol, primaryexchange, industry, sector } = company; 
-  const [chartFields, setChartFields] = useState('totalrevenue')
-  return (
-    <div className={classes.root}>
-      <Grid container 
-        direction="row"
-        justify="center"
-        
-        spacing={24}
-      >
-        <Grid item xl={9} lg={10} md={10} s={12} xs={12}> 
-          <FavoriteButton companyId={company.id} />
-            {companyname}
-            {primarysymbol}
-            {primaryexchange}
-            {industry}
-            {sector}
-          <button onClick={()=> setChartFields('ebit')} name='ebit'>Show EBIT</button>
-          <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-            <LineChart years={years} data={data} fieldNames={chartFields} displayName={'Revenue'}/>
-          </Grid>
-          <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-          <Statement years={years} fields={fields['iS']} data={data}/>
-          </Grid>
-          <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-            <Statement years={years} fields={fields['bS']} data={data}/>
-          </Grid>
-          <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-            <Statement years={years} fields={fields['cF']} data={data}/>
+      <div className={classes.root}>
+        <Grid container 
+          direction="row"
+          justify="center"
+          spacing={24}
+        >
+          <Grid item xl={9} lg={10} md={10} s={12} xs={12}> 
+            <FavoriteButton companyId={company.id} />
+              {companyname}
+              {primarysymbol}
+              {primaryexchange}
+              {industry}
+              {sector}
+            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
+              <LineChart years={years} data={data} fieldNames={'totalrevenue'} displayName={'Revenue'}/>
+            </Grid>
+            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
+            <Statement years={years} fields={fields['iS']} data={data}/>
+            </Grid>
+            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
+              <Statement years={years} fields={fields['bS']} data={data}/>
+            </Grid>
+            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
+              <Statement years={years} fields={fields['cF']} data={data}/>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </div>
-  )
-  
+      </div>
+    )
+  }
 }
 const mapState = (state, ownProps) => {
   const companyId = parseInt(ownProps.companyId, 10);
@@ -88,5 +94,8 @@ const mapState = (state, ownProps) => {
     error: state.companies.loadingStmntError
   }
 }
+const mapDispatch = { 
+  loadStatements
+}
 
-export default withStyles(styles)(connect(mapState, { loadStatements })(CompanyProfilePage));
+export default withStyles(styles)(connect(mapState, mapDispatch)(CompanyProfilePage));
