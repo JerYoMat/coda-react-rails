7ab8aabfd2a1dbf088197bda5fa7df4d5b2c3ca1
JerYOMat <jeremy.emata@gmail.com>
2019-05-13 23:49:11 -0400

    cleared out -> rewrite

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import SearchComponent from '../components/search/SearchComponent';
+
 const HomePage = () => (
   <div>  
-    <div className='banner-container'>
-      <div className='welcome-card col-md-7 offset-md-2'>
-        <h2>Welcome to CoDA</h2>
-        <ul>
-          <li>Search companies listed on NYSE</li>
-          <li>Receive Altman Z-Score, Stock Price and Financial Data</li>
-          <li>Follow Companies</li>
-          <li>Customize your Data</li>
-        </ul>
-      </div>
-    </div>
-    <SearchComponent />
+    
   </div>
 )
 
