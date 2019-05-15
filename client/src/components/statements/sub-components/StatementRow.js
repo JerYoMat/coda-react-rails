fa7cd392973f2a26d5ec992cdd3b068035b2e166
JerYOMat <jeremy.emata@gmail.com>
2019-05-15 12:27:11 -0400

    removed padding from table data -may need to put this back

--- client/src/components/statements/sub-components/StatementRow.js
+++ client/src/components/statements/sub-components/StatementRow.js
@@ -5,10 +5,10 @@ import TableRow from '@material-ui/core/TableRow';
 const StatementRow = ({ years, fieldKey, data, field}) => {
   return (
     <TableRow>
-      <TableCell padding='dense'>{field[0]}</TableCell>
+      <TableCell padding='none'>{field[0]}</TableCell>
       {
         years.map(year => (
-          <TableCell padding='dense' align='right' key={`${fieldKey}-${year}`}>{data[year][fieldKey]}</TableCell>
+          <TableCell padding='none' align='right' key={`${fieldKey}-${year}`}>{data[year][fieldKey]}</TableCell>
       ))
       }
     </TableRow>
