9779c60ec89e777fca929abfa1effa7f1b1c606d
JerYOMat <jeremy.emata@gmail.com>
2019-05-15 12:19:17 -0400

    added charts, added grid

--- client/src/pages/CompanyProfilePage.js
+++ client/src/pages/CompanyProfilePage.js
@@ -1,41 +1,89 @@
-import React,{ Component } from 'react';
-import { connect } from 'react-redux';
-import { loadStatements } from '../actions';
-import { withStyles } from '@material-ui/core/styles';
-import Statement from '../components/statements/Statement';
-import LineChart from '../components/charts/LineChart';
-import FavoriteButton from '../components/favorites/FavoriteButton';
-import Loading from '../components/misc/Loading';
-import Grid from '@material-ui/core/Grid';
-import NotFoundPage from './NotFoundPage';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { loadStatements, loadStockData } from "../actions";
+import { withStyles } from "@material-ui/core/styles";
+import Statement from "../components/statements/Statement";
+import LineChart from "../components/charts/LineChart";
+import StockChart from "../components/charts/StockChart";
+import FavoriteButton from "../components/favorites/FavoriteButton";
+import Loading from "../components/misc/Loading";
+import Grid from "@material-ui/core/Grid";
+import NotFoundPage from "./NotFoundPage";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    marginTop: "1rem",
+    flexGrow: 1
   },
-  paper: {
-    textAlign: 'center',
+  card: {
+    padding: theme.spacing.unit * 2,
     color: theme.palette.text.secondary,
+    marginTop: "1rem"
   },
+  fin: {
+    color: theme.palette.primary.light
+  },
+  market: {
+    color: theme.palette.secondary.light
+  },
+  button: {
+    position: "relative",
+    float: "right",
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
+  }
 });
 
 class CompanyProfilePage extends Component {
   componentDidMount() {
-    const { data, company, loadStatements  } = this.props
-    console.log(data)
-    console.log(company)
-    if (!data) {
-      loadStatements(company.id)
+    const {
+      data,
+      company,
+      loadStatements,
+      loadStockData,
+      loading,
+      stockData,
+      stocksLoading
+    } = this.props;
+    if (!data && !loading) {
+      loadStatements(company.id);
+    }
+    if (!stockData && !stocksLoading) {
+      loadStockData(company.id);
     }
   }
   componentDidUpdate(prevProps) {
-    const { company, data, loadStatements } = this.props
-    if (!data) {
-     loadStatements(company.id)
+    const {
+      data,
+      company,
+      loadStatements,
+      loading,
+      loadStockData,
+      stockData,
+      stocksLoading
+    } = this.props;
+    if (!data && !loading) {
+      loadStatements(company.id);
+    }
+    if (!stockData && !stocksLoading) {
+      loadStockData(company.id);
     }
   }
   render() {
-    const { classes, company, data, fields, loading, error} = this.props
+    const {
+      classes,
+      company,
+      data,
+      fields,
+      loading,
+      error,
+      stocksLoading,
+      stockData
+    } = this.props;
     if (loading || !data) {
       return (
         <div>
@@ -48,54 +96,163 @@ class CompanyProfilePage extends Component {
       return <NotFoundPage />;
     }
     if (error) {
-      return <div>Error: {error.message}</div>
+      return <div>Error: {error.message}</div>;
     }
-    const years = Object.keys(data)
-    const { companyname, primarysymbol, primaryexchange, industry, sector } = company; 
+    const years = Object.keys(data);
+    const {
+      id,
+      companyname,
+      primarysymbol,
+      primaryexchange,
+      industry,
+      sector
+    } = company;
     return (
       <div className={classes.root}>
-        <Grid container 
-          direction="row"
-          justify="center"
-          spacing={24}
-        >
-          <Grid item xl={9} lg={10} md={10} s={12} xs={12}> 
-            <FavoriteButton companyId={company.id} />
-              {companyname}
-              {primarysymbol}
-              {primaryexchange}
-              {industry}
-              {sector}
-            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-              <LineChart years={years} data={data} fieldNames={'totalrevenue'} displayName={'Revenue'}/>
-            </Grid>
-            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-            <Statement years={years} fields={fields['iS']} data={data}/>
-            </Grid>
-            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-              <Statement years={years} fields={fields['bS']} data={data}/>
-            </Grid>
-            <Grid item xl={9} lg={10} md={11} s={12} xs={12} >
-              <Statement years={years} fields={fields['cF']} data={data}/>
-            </Grid>
+        <Grid container spacing={24}>
+          <Grid item lg={6} xs={12}>
+            <Card className={classes.card}>
+              <Typography variant="h3">{companyname}</Typography>
+              <div>
+                <Typography
+                  inline={true}
+                  variant="button"
+                  className={classes.test}
+                >
+                  Ticker:{"\u00A0"}
+                </Typography>
+                <Typography
+                  inline={true}
+                  variant="subtitle2"
+                  className={classes.market}
+                >
+                  {primarysymbol}
+                </Typography>
+                <span> | </span>
+                <Typography
+                  inline={true}
+                  variant="button"
+                  className={classes.mainText}
+                >
+                  Exchange:{"\u00A0"}
+                </Typography>
+                <Typography
+                  className={classes.market}
+                  inline={true}
+                  variant="subtitle2"
+                >
+                  {primaryexchange}
+                </Typography>
+              </div>
+              <div>
+                <Typography
+                  inline={true}
+                  variant="button"
+                  className={classes.test}
+                >
+                  Industry:{"\u00A0"}
+                </Typography>
+                <Typography
+                  inline={true}
+                  variant="subtitle1"
+                  className={classes.test}
+                >
+                  {industry}
+                </Typography>
+              </div>
+              <div>
+                <Typography inline={true} variant="button">
+                  Sector:{"\u00A0"}
+                </Typography>
+
+                <Typography
+                  inline={true}
+                  variant="subtitle1"
+                  className={classes.test}
+                >
+                  {sector}
+                </Typography>
+                <FavoriteButton companyId={company.id} />
+              </div>
+            </Card>
+            <Card className={classes.card}>
+              <Typography className={classes.fin} variant="subtitle2">
+                Income Statement:
+              </Typography>
+              <Statement years={years} fields={fields["iS"]} data={data} />
+            </Card>
+
+            <Card className={classes.card}>
+              <Typography className={classes.fin} variant="subtitle2">
+                Balance Sheet:
+              </Typography>
+              <Statement years={years} fields={fields["bS"]} data={data} />
+            </Card>
+
+            <Card className={classes.card}>
+              <Typography className={classes.fin} variant="subtitle2">
+                Cash Flow Statement:
+              </Typography>
+              <Statement years={years} fields={fields["cF"]} data={data} />
+            </Card>
+          </Grid>
+          <Grid item lg={6} xs={12}>
+            <Card className={classes.card}>
+              <Typography className={classes.fin} variant="subtitle2">
+                Reporting Trends
+              </Typography>
+              <LineChart
+                years={years}
+                data={data}
+                fieldNames={"totalrevenue"}
+                displayName={"Revenue"}
+              />
+            </Card>
+
+            {stocksLoading && <Loading />}
+            {stockData && (
+              <Card className={classes.card}>
+                <Typography
+                  className={classes.market}
+                  variant="subtitle2"
+                  color="secondary"
+                >
+                  Market Data
+                </Typography>
+
+                <StockChart
+                  data={stockData}
+                  ticker={primarysymbol}
+                  exchange={primaryexchange}
+                />
+              </Card>
+            )}
           </Grid>
         </Grid>
       </div>
-    )
+    );
   }
 }
 const mapState = (state, ownProps) => {
   const companyId = parseInt(ownProps.companyId, 10);
   return {
-    company:state.companies.list.find(el => el.id === companyId),
+    company: state.companies.list.find(el => el.id === companyId),
     data: state.companies.statements[companyId],
     fields: state.user.customFields,
     loading: state.companies.loadingStmnt,
-    error: state.companies.loadingStmntError
-  }
-}
-const mapDispatch = { 
-  loadStatements
-}
+    error: state.companies.loadingStmntError,
+    stockData: state.companies.stockPrices[companyId],
+    stocksLoading: state.companies.stocksLoading
+  };
+};
+const mapDispatch = {
+  loadStatements,
+  loadStockData
+};
 
-export default withStyles(styles)(connect(mapState, mapDispatch)(CompanyProfilePage));
+export default withStyles(styles)(
+  connect(
+    mapState,
+    mapDispatch
+  )(CompanyProfilePage)
+);
