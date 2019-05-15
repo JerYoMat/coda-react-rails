10db322d046d5662d97be800a43a62e0869efe16
JerYOMat <jeremy.emata@gmail.com>
2019-05-15 12:20:52 -0400

    added url_encode for ticker alphaVantage request

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,12 +1,14 @@
 require 'rest-client'
 require 'json'
+require 'erb'
+include ERB::Util
 class Company < ApplicationRecord
   include ActionView::Helpers::NumberHelper
   has_many :favorites
   has_many :financials  
 
   def get_stock_data
-    url = ENV['ALPHA_URL']+"?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=#{self.primarysymbol}&apikey=#{ENV['ALPHA_KEY']}"
+    url = ENV['ALPHA_URL']+"?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=#{url_encode(self.primarysymbol)}&apikey=#{ENV['ALPHA_KEY']}"
     response = RestClient::Request.execute(
       method: :get,
       url: url
