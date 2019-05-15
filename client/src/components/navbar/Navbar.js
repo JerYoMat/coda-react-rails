a0a24729cc36c0dd98e95bdc9ccbc3f05677c32a
JerYOMat <jeremy.emata@gmail.com>
2019-05-15 12:22:53 -0400

    formatting

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,120 +1,121 @@
-import React, { useState } from 'react';
-import { logout, openLoginForm, closeLoginForm } from '../../actions';
-import { connect } from 'react-redux';
-import { Link as RouterLink, navigate } from '@reach/router';
-import Logo from '../navbar/Logo';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import './Navbar.scss';
-import Link from '@material-ui/core/Link';
-import Search from '../search/Search';
-import { fade } from '@material-ui/core/styles/colorManipulator';
-import Modal from 'react-modal';
-import LoginWrapper from '../login/LoginWrapper';
-
-
+import React, { useState } from "react";
+import { logout, openLoginForm, closeLoginForm } from "../../actions";
+import { connect } from "react-redux";
+import { navigate } from "@reach/router";
+import Logo from "../navbar/Logo";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import "./Navbar.scss";
+import Search from "../search/Search";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import Modal from "react-modal";
+import LoginWrapper from "../login/LoginWrapper";
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%"
   },
   grow: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   appBar: {
-    height: '52px'
+    height: "52px"
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20,
+    marginRight: 20
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
     },
     marginRight: theme.spacing.unit * 2,
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing.unit * 3,
-      width: 'auto',
-    },
+      width: "auto"
+    }
   },
   searchIcon: {
     width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
   inputRoot: {
-    color: 'inherit',
-    width: '100%',
+    color: "inherit",
+    width: "100%"
   },
   inputInput: {
     paddingTop: theme.spacing.unit,
     paddingRight: theme.spacing.unit,
     paddingBottom: theme.spacing.unit,
     paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
   },
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
   },
   sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  }
 });
 
-
-
-
-const Navbar = ({ classes, user, logout, modalOpen, openLoginForm, closeLoginForm }) => {
+const Navbar = ({
+  classes,
+  user,
+  logout,
+  modalOpen,
+  openLoginForm,
+  closeLoginForm
+}) => {
   const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl)
-  
+  const open = Boolean(anchorEl);
+
   const goToSettings = () => {
-     navigate(`/users/${user.id}`);
-     setAnchorEl(null)
-  }
+    navigate(`/users/${user.id}`);
+    setAnchorEl(null);
+  };
   const goToRoot = () => {
     navigate(`/`);
     setAnchorEl(null);
-  }
+  };
 
   const handleLogOut = () => {
-    setAnchorEl(null)
-    logout()
-    goToRoot()
-  }
+    setAnchorEl(null);
+    logout();
+    goToRoot();
+  };
 
   return (
     <React.Fragment>
@@ -174,11 +175,7 @@ const Navbar = ({ classes, user, logout, modalOpen, openLoginForm, closeLoginFor
             )}
 
             {!user && (
-              <Typography
-                color="primary"
-                variant="h6"
-                onClick={openLoginForm}
-              >
+              <Typography color="primary" variant="h6" onClick={openLoginForm}>
                 Login
               </Typography>
             )}
@@ -203,15 +200,18 @@ const Navbar = ({ classes, user, logout, modalOpen, openLoginForm, closeLoginFor
       </Modal>
     </React.Fragment>
   );
-}
+};
 
 const mapState = state => ({
   user: state.user.info,
   modalOpen: state.user.modalOpen
-})
+});
 
-const mapDispatch = { logout, openLoginForm, closeLoginForm }
+const mapDispatch = { logout, openLoginForm, closeLoginForm };
 
-
-export default withStyles(styles)(connect(mapState, mapDispatch)(Navbar));
- 
+export default withStyles(styles)(
+  connect(
+    mapState,
+    mapDispatch
+  )(Navbar)
+);
