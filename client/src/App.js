a749b4a91adf1852a4fc9e098d9b97cbbaa29b4f
JerYOMat <jeremy.emata@gmail.com>
2019-05-17 21:47:42 -0400

    removed tutorial component and replaced with simple search bar -tutorial component was confusing users

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import HomePage from './pages/HomePage';
+import TutorialPage from './pages/TutorialPage';
 import CompanyProfilePage from './pages/CompanyProfilePage';
 import UserPage from './pages/UserPage';
 import Navbar from './components/navbar/Navbar';
-
+import HomePage from './pages/HomePage';
 
 const App = () => (
   <div>
     <Navbar />
     <Router>
       <HomePage path='/' />
+      <TutorialPage path='/tutorial' />
       <CompanyProfilePage path='/companies/:companyId' />
       <UserPage path='/users/:userId' />
     </Router>
